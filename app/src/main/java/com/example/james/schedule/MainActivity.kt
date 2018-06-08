2839d25139bef67c733e91207998b591f22cdce4
James <jmb47580@ucmo.edu>
2018-06-08 11:07:01 -0500

    Added OnClickListener to MySchedule Button

--- app/src/main/java/com/example/james/schedule/MainActivity.kt
+++ app/src/main/java/com/example/james/schedule/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.example.james.schedule
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Fragment
+import android.app.FragmentTransaction
+import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v4.content.ContextCompat.startActivity
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.view.View
+import android.R.menu
+import android.content.DialogInterface
+import android.view.Menu
+import com.example.james.schedule.R.layout.activity_main
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Get the view from activity_main.xml
         setContentView(R.layout.activity_main)
-    }
+
+        // Locate the button in activity_main.xml
+        val button = findViewById<Button>(R.id.Button1)
+
+
+
+        button.setOnClickListener( {
+            button.setOnClickListener{
+                startActivity(Intent(this@MainActivity, SchedulePage::class.java))
+            }
+
+
+            })
+        }
+
 }
+
+
+
---
+++ app/src/main/java/com/example/james/schedule/SchedulePage.kt
@@ -0,0 +1,23 @@
+package com.example.james.schedule
+
+import android.app.Fragment
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewParent
+import kotlinx.android.synthetic.main.schedule_main.*
+
+class SchedulePage : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.schedule_main)
+
+    }
+    //override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int):
+}
