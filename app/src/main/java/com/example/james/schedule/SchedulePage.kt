9c691551542f557f8fa4419e547ffd4f61fb14ef
James <jmb47580@ucmo.edu>
2018-06-27 12:34:53 -0500

    Changed some layout and added textView

--- app/src/main/java/com/example/james/schedule/SchedulePage.kt
+++ app/src/main/java/com/example/james/schedule/SchedulePage.kt
@@ -11,10 +11,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.ViewParent
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.schedule_main.*
 
-class SchedulePage : AppCompatActivity() {
+class SchedulePage : AppCompatActivity(), View.OnClickListener {
 
+    lateinit var editText: EditText
+    lateinit var button2: Button
+    lateinit var textViewMessage: TextView
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,16 +28,23 @@ class SchedulePage : AppCompatActivity() {
         setContentView(R.layout.schedule_main)
 
         // Locate the button in activity_main.xml
-        val button2 = findViewById<Button>(R.id.btn_calender)
+        //val button2 = findViewById<Button>(R.id.btn_calender)
+        button2 = findViewById(R.id.btn_calender)
+        editText = findViewById(R.id.editText)
+        textViewMessage = findViewById(R.id.lv_android)
 
+        //sets button to intent which directs it to --> CalendarPage
+        //button2.setOnClickListener( {
+           // button2.setOnClickListener{
+                //startActivity(Intent(this@SchedulePage, MainActivity::class.java))
+           // }
 
-        //sets button to intent which directs it to --> SchedulePage
-        button2.setOnClickListener( {
-            button2.setOnClickListener{
-                startActivity(Intent(this@SchedulePage, MainActivity::class.java))
-            }
+       // })
+        button2.setOnClickListener(this)
+    }
+    override fun onClick(view: View?){
+        val peach = editText.text
+        textViewMessage.text = "Your schedule is $peach"
 
-
-        })
     }
 }
