4eba068e0da672fcf6522f8a3f5a28ee75c99ad4
James <jmb47580@ucmo.edu>
2018-06-28 02:00:11 -0500

    Put in an adapter for the week.

--- app/src/main/java/com/example/james/schedule/WeekActivity.java
+++ app/src/main/java/com/example/james/schedule/WeekActivity.java
@@ -1,7 +1,11 @@
 package com.example.james.schedule;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.support.v7.widget.Toolbar;
 
@@ -31,5 +35,19 @@ public class WeekActivity extends AppCompatActivity {
 
     private void setupListView(){
         String[] week = getResources().getStringArray(R.array.Week);
+
+        WeekAdapter adapter = new WeekAdapter(this, R.layout.main_activity_single, week); //might be a different layout here
+    }
+    public class WeekAdapter extends ArrayAdapter{
+        private int resource;
+        private LayoutInflater layoutInflater;
+        private String[] week = new String[]{};
+
+        public WeekAdapter(Context context, int resource, String[] objects){
+            super(context, resource, objects);
+            this.resource = resource;
+            this.week = objects;
+            layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
     }
 }
