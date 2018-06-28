54f3d735d904ea4e53355ba958aa9c480c959d15
James <jmb47580@ucmo.edu>
2018-06-28 02:16:29 -0500

    Set up the ViewHolder and a check for it

--- app/src/main/java/com/example/james/schedule/WeekActivity.java
+++ app/src/main/java/com/example/james/schedule/WeekActivity.java
@@ -2,12 +2,20 @@ package com.example.james.schedule;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Layout;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.w3c.dom.Text;
 
 public class WeekActivity extends AppCompatActivity {
 
@@ -48,6 +56,27 @@ public class WeekActivity extends AppCompatActivity {
             this.resource = resource;
             this.week = objects;
             layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, @android.support.annotation.Nullable View convertView, @NonNull ViewGroup parent) {
+            ViewHolder holder;
+            if(convertView == null){
+                holder = new ViewHolder();
+                convertView = layoutInflater.inflate(resource, null);
+                holder.ivLogo = (LetterImageView)convertView.findViewById(R.id.ivLetter);
+                holder.tvWeek = (TextView)convertView.findViewById(R.id.tvMain); //not sure about id?
+                convertView.setTag(holder);
+            }else{
+                holder = (ViewHolder)convertView.getTag();
+            }
+            return convertView;
+        }
+        class ViewHolder{
+            private LetterImageView ivLogo;
+            private TextView tvWeek;
+
+
         }
     }
 }
